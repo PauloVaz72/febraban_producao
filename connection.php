717ded28b943d08239ac6b4d2e2e0f51c620dd9a
Gabriel Lencina <gabriellencina831@gmail.com>
2022-12-13 16:12:13 -0300

    ultima versão do codigo

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 <?php
-// define ('HOST','mysql.intranetfuturo.com.br');
-// define ('USER','intranetfuturo04');
-// define ('PASS','Futuro2022');
-// define ('BASE','intranetfuturo04');  
+define ('HOST','mysql.intranetfuturo.com.br');
+define ('USER','intranetfuturo04');
+define ('PASS','Futuro2022');
+define ('BASE','intranetfuturo04');  
 
 
-define ('HOST','localhost');
-define ('USER','root');
-define ('PASS','16062001');
-define ('BASE','intranet');  
+// define ('HOST','localhost');
+// define ('USER','root');
+// define ('PASS','16062001');
+// define ('BASE','intranet');  
 
 $connection = new mysqli(HOST, USER, PASS, BASE); 
